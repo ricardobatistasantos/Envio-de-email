2f330c34fa37699350dc841c35ede6a8c90c4769
Ricardo Santos <ricardo.batista.s@somoseducacao.com.br>
2025-10-20 17:50:31 -0300

    structure more clear

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './presentation/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule);
+(async () => {
+  await NestFactory.createApplicationContext(AppModule);
   console.log('Email Worker iniciado e escutando a fila...');
-}
-
-bootstrap();
+})()
